6f69292770664190236d2a7972bf6bcbc608a64d
Alain Kalker <a.c.kalker@gmail.com>
2014-03-18 03:52:18 +0100

    Fix nodewebkit:linux task

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
                     zip: false
                 },
                 src: [
-                    ".tmp/**"
+                    "./**"
                 ]
             }
         },
