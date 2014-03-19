12f77e0ebf451420fbf04ba9fd30ac218f2a86e1
Samy Pessé <samypesse@gmail.com>
2014-03-19 16:15:09 -0700

    Add bugs (pty) to rebuild with nw-gyp

--- scripts/nwbuild.sh
+++ scripts/nwbuild.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 NW_VERSION=$1
 NW_VERSION=${NW_VERSION:=0.8.0}
-REBUILD_MODULES=("codebox/node_modules/shux/node_modules/pty.js" "codebox/node_modules/ctags" "codebox/node_modules/gittle/node_modules/pty.js" "codebox/node_modules/vfs-local/node_modules/pty.js")
+REBUILD_MODULES=("codebox/node_modules/shux/node_modules/pty.js" "codebox/node_modules/bugs/node_modules/pty.js" "codebox/node_modules/ctags" "codebox/node_modules/gittle/node_modules/pty.js" "codebox/node_modules/vfs-local/node_modules/pty.js")
 
 echo $NW_VERSION
 function rebuild_module () {
